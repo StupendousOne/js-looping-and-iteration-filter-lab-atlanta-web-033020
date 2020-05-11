d7e4082ce23edfa41e8b8096119476888e924386
StupendousOne <harrisongeo87@gmail.com>
2020-05-10 23:59:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(arr, text){
+    let retArr = []
+    for(const element of arr){
+        if(element.toLowerCase() === text.toLowerCase()){
+            retArr.push(element)
+        }
+    }
+    return retArr
+}
+
+function fuzzyMatch(arr, text){
+    let retArr = []
+    for(const element of arr){
+        if(element.slice(0,text.length).toLowerCase() === text.toLowerCase()){
+            retArr.push(element)
+        }
+    }
+    return retArr
+}
+
+function matchName(arr,text){
+    let retArr = []
+    for(const element of arr){
+        if(element.name.toLowerCase() === text.toLowerCase()){
+            retArr.push(element)
+        }
+    }
+    return retArr
+}
